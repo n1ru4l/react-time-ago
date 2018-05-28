ebd41a6c854cc8b7b74241ee220319c6ce591038
Laurin Quast <laurinquast@googlemail.com>
2018-05-28 22:25:38 +0200

    test(jest): remove jest-each dependency
    jest-each is now part of the jest core

--- src/index.test.js
+++ src/index.test.js
@@ -1,4 +1,3 @@
-import each from 'jest-each'
 import lolex from 'lolex'
 import { createElement, Component } from 'react'
 import Enzyme, { mount } from 'enzyme'
@@ -11,21 +10,22 @@ Enzyme.configure({ adapter: new EnzymeAdapter() })
 describe(`formatDate`, () => {
   const initial = Math.floor(new Date().getTime() / 1000)
 
-  each([
-    [initial, initial, `now`, 120],
-    [initial, initial + 119, `now`, 1],
-    [initial, initial + 120, `2m ago`, 60],
-    [initial, initial + 121, `2m ago`, 59],
-    [initial, initial + 60 * 59, `59m ago`, 60],
-    [initial, initial + 60 * 59 + 59, `59m ago`, 1],
-    [initial, initial + 60 * 60, `1h ago`, 3600],
-    [initial, initial + 60 * 60 * 2 - 1, `1h ago`, 1],
-    [initial, initial + 60 * 120, `2h ago`, 3600],
-    [initial, initial + 60 * 60 * 24 - 1, `23h ago`, 1],
-    [initial, initial + 60 * 60 * 24, `1d ago`, 60 * 60 * 24],
-    [initial, initial + 60 * 60 * 24 * 2, `2d ago`, 60 * 60 * 24],
-    [initial, initial + 60 * 60 * 24 * 2 + 3, `2d ago`, 60 * 60 * 24 - 3],
-  ]).test(
+  test.each(
+    [
+      [initial, initial, `now`, 120],
+      [initial, initial + 119, `now`, 1],
+      [initial, initial + 120, `2m ago`, 60],
+      [initial, initial + 121, `2m ago`, 59],
+      [initial, initial + 60 * 59, `59m ago`, 60],
+      [initial, initial + 60 * 59 + 59, `59m ago`, 1],
+      [initial, initial + 60 * 60, `1h ago`, 3600],
+      [initial, initial + 60 * 60 * 2 - 1, `1h ago`, 1],
+      [initial, initial + 60 * 120, `2h ago`, 3600],
+      [initial, initial + 60 * 60 * 24 - 1, `23h ago`, 1],
+      [initial, initial + 60 * 60 * 24, `1d ago`, 60 * 60 * 24],
+      [initial, initial + 60 * 60 * 24 * 2, `2d ago`, 60 * 60 * 24],
+      [initial, initial + 60 * 60 * 24 * 2 + 3, `2d ago`, 60 * 60 * 24 - 3],
+    ],
     `returns the correct value for the start date %s and the current date %s which is '%s' and the next update in %s seconds`,
     (date, now, value, next) => {
       expect(formatDate(initial, now)).toEqual({
