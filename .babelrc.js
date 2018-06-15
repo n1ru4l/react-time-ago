393762878593baae455bc05aa91daf53c790420b
Laurin Quast <laurinquast@googlemail.com>
2018-06-15 18:10:11 +0200

    build(flow): strip flow types from distribution code

--- .babelrc.js
+++ .babelrc.js
@@ -1,17 +1,21 @@
 "use strict";
 
 module.exports = {
-  presets: [
-    [
-      "@babel/preset-env",
-      {
-        targets: {
-          browsers: "last 2 versions",
-          node: "8"
-        },
-        modules: process.env.NODE_ENV === "test" ? undefined : false
-      }
-    ]
-  ],
+  presets:
+    process.env.NODE_ENV !== "build-flow"
+      ? [
+          [
+            "@babel/preset-env",
+            {
+              targets: {
+                browsers: "last 2 versions",
+                node: "8"
+              },
+              modules: process.env.NODE_ENV === "test" ? undefined : false,
+              loose: true
+            }
+          ]
+        ]
+      : [],
   plugins: ["@babel/plugin-transform-flow-strip-types"]
 };
